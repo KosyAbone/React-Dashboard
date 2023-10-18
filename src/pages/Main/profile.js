e61d14374a2cb4b40e61a31fe23a3f50f87279fc
Ilham Sheikh <ilhamsheikh@Ilhams-MacBook-Pro.local>
2023-10-18 15:54:23 -0400

    added input lat,long feature to map.js

--- src/pages/Main/profile.js
+++ src/pages/Main/profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Sidebar from '../../components/SideBar/sideBar';
 import { Outlet } from 'react-router-dom';
 import './profile.css';
--- src/pages/SidePages/Map/map.js
+++ src/pages/SidePages/Map/map.js
@@ -1,54 +1,56 @@
-import React, { useState } from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "./map.css";
 import "leaflet/dist/leaflet.css";
-import { Icon, L } from "leaflet";
-import { geocodeService } from 'esri-leaflet';
+import { Icon } from "leaflet";
 
 const Map = () => {
-    const [location, setLocation] = useState([51.505, -0.09]);
-    const [searchLocation, setLocationValue] = useState('');
+    const [latitude, setLatitude] = useState(67.59);
+    const [longitude, setLongitude] = useState(58.67);
+    const [location, setLocation] = useState([latitude, longitude]);
+    const [mapKey, setMapKey] = useState("mapKey");
 
     const customIcon = new Icon({
         iconUrl : require("../../../assets/location-pin.png"),
         iconSize : [38, 38]
     })
 
-//     useEffect(() => {
-//     const handleSearch = () => {
-//       const geocodeService = L.esri.Geocoding.geocodeService();
-      
-//       geocodeService.geocode().text(searchValue).run(function (error, results) {
-//         if (!error && results.results.length > 0) {
-//           const { lat, lng } = results.results[0].latlng;
-//           setLocation([lat, lng]);
-//         }
-//       });
-//     };
-//   }, []);
+    useEffect(() => {
+        // Update the map center when the location changes
+        setLocation([latitude, longitude]);
+    }, [latitude, longitude]);
 
-const handleSearch = () => {
-    const geocodeService = L.esri.Geocoding.geocodeService();
-      
-    geocodeService.text(searchLocation).run(function (error, results) {
-        if (!error && results.results.length > 0) {
-            const { lat, lng } = results.results[0].latlng;
-            setLocation([lat, lng]);
-        }
-    });
-};
+
+    const handleSearch = () => {
+        setLocation([latitude, longitude]);
+        setMapKey(Date.now().toString());
+    };
 
     return (
     <div style={{maxWidth: '100%' }}>
         <div className="location-search">
             <h1>Search Location</h1>
-            <input type="text" value={searchLocation} onChange={(e) => setLocationValue(e.target.value)} placeholder="Search Location" />
-            <button type="submit">Search</button>
+            <label className="location-input">
+                Latitude
+                <input type="number" value={latitude} onChange={(e) => setLatitude(parseFloat(e.target.value))} placeholder="Enter Latitude" />
+            </label>
+            <label className="location-input">
+                Longitude
+                <input type="number" value={longitude} onChange={(e) => setLongitude(parseFloat(e.target.value))} placeholder="Enter Longitude" />
+            </label>
+            <button className="search-button" type="submit" onClick={handleSearch}>Search</button>
         </div>
-      <MapContainer center={location} zoom={13} scrollWheelZoom={false}>
+        <div className="location-print">
+            {location ? (
+                <h2>Location: {location[0]}, {location[1]}</h2>
+            ) : (
+                <h2>Enter the Latitude and Longitude Please</h2>
+            )}
+        </div>
+      <MapContainer key={mapKey} center={location} zoom={13} scrollWheelZoom={false}>
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <Marker position={[51.505, -0.09]} icon={customIcon}>
+                <Marker position={[latitude, longitude]} icon={customIcon}>
                     <Popup>
                         A pretty popup {location} <br /> Easily customizable.
                     </Popup>
