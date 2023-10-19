d9d44a17cfb7d65165722d591cce3619c16390d4
Kosiso Abone <abonekosiso@gmail.com>
2023-10-18 22:40:54 -0400

    patch

--- src/components/SideBar/sideBar.js
+++ src/components/SideBar/sideBar.js
@@ -7,7 +7,7 @@ const Sidebar = ({ onItemClick }) => {
     <div className="sidebar">
         <Link to="/login/dashboard" className="sidebar-item" onClick={() => onItemClick('DashBoard')}>Profile</Link>
         <Link to="/login/map" className="sidebar-item" onClick={() => onItemClick('Map')}>Map</Link>
-        <Link to="/login/news-feed" className="sidebar-item" onClick={() => onItemClick('Checklist')}>News Feed</Link>
+        <Link to="/login/news-feed" className="sidebar-item" onClick={() => onItemClick('NewsFeed')}>News Feed</Link>
         <Link to="/login/weather" className="sidebar-item" onClick={() => onItemClick('Weather')}>Weather</Link>
         <Link to="/login/tools" className="sidebar-item" onClick={() => onItemClick('Tools')}>Tools</Link>
         <button className='logoutBtn'>Logout</button>
