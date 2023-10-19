5e7f521ce77baac0161ada55e2bdd46cad7874fe
Kosiso Abone <abonekosiso@gmail.com>
2023-10-19 19:49:45 -0400

    set up retrieval of logged in user profile information

--- src/pages/private/Profile/profile.js
+++ src/pages/private/Profile/profile.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { auth } from "../../../firebase";
 
 const Profile = () => {
+    const user = auth.currentUser;
+    let userName = '';
+    let userEmail = '';
+
+    if (user) {
+        userName = user.displayName; // User's name
+        userEmail = user.email; // User's email address
+    }
+
     return (
         <div>
-        <h1>Profile</h1>
-        <p>Welcome to the Profile page!</p>
+            <h1>User Profile Information</h1>
+            <p>Name: {userName}</p>
+            <p>Email Address: {userEmail}</p>
         </div>
     );
 }
