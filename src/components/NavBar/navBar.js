86085b0c260b738ce03fce37c59c0dfbe8e5fad2
Kosiso Abone <abonekosiso@gmail.com>
2023-10-19 02:16:50 -0400

    modified folder structure, added login function

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -4,12 +4,17 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className="navbar">
-        <Link to="/" className="nav-item"><i className="fas fa-home"></i>Home</Link>
-        <Link to="/about" className="nav-item"><i className="fas fa-cog"></i>About</Link> 
-        <Link to="/notifications" className="nav-item"><i className="fas fa-bell"></i>Users</Link>
-        <Link to="/login" className="nav-item"><i className="fas fa-user"></i>Profile</Link>
-    </nav>
+    <header className="header">
+      <div className="logo">
+        <h1>Xplorer</h1>
+      </div>
+      <nav className="navbar">
+          <Link to="/" className="nav-item"><i className="fas fa-home"></i>Home</Link>
+          <Link to="/about" className="nav-item"><i className="fas fa-cog"></i>About</Link> 
+          <Link to="/users" className="nav-item"><i className="fas fa-bell"></i>Users</Link>
+          <Link to="/login" className="nav-item"><i className="fas fa-user"></i>Account</Link>
+      </nav>
+    </header>
   );
 };
 
--- src/components/SideBar/sideBar.js
+++ src/components/SideBar/sideBar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './sideBar.css';
 import { Link } from 'react-router-dom';
+import { useUserContext } from '../../context/userContext'; 
 
 const Sidebar = ({ onItemClick }) => {
+  const { logoutUser } = useUserContext();
   return (
     <div className="sidebar">
-        <Link to="/login/dashboard" className="sidebar-item" onClick={() => onItemClick('DashBoard')}>Profile</Link>
-        <Link to="/login/map" className="sidebar-item" onClick={() => onItemClick('Map')}>Map</Link>
-        <Link to="/login/news-feed" className="sidebar-item" onClick={() => onItemClick('NewsFeed')}>News Feed</Link>
-        <Link to="/login/weather" className="sidebar-item" onClick={() => onItemClick('Weather')}>Weather</Link>
-        <Link to="/login/tools" className="sidebar-item" onClick={() => onItemClick('Tools')}>Tools</Link>
-        <button className='logoutBtn'>Logout</button>
+        <Link to="/login/profile" className="sidebar-item">Profile</Link>
+        <Link to="/login/map" className="sidebar-item" >Map</Link>
+        <Link to="/login/news-feed" className="sidebar-item">News Feed</Link>
+        <Link to="/login/weather" className="sidebar-item">Weather</Link>
+        <Link to="/login/tools" className="sidebar-item">Tools</Link>
+        <button className='logoutBtn' onClick={() => logoutUser()}>Logout</button>
     </div>
   );
 };
