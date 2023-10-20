668d3bef93b18ca7469400330ed2506f6279ec7a
Kosiso Abone <abonekosiso@gmail.com>
2023-10-20 07:58:42 -0400

    designed error page

--- src/pages/public/Login/login.js
+++ src/pages/public/Login/login.js
@@ -2,11 +2,13 @@ import React from "react";
 import { useUserContext } from "../../../context/userContext";
 import Auth from "../../../context/auth/auth";  
 import Dashboard from "../../private/Dashboard/dashboard";
+import './login.css';
 
 const Login = () => {   
     const { loading, error, user } = useUserContext();
+    console.log(error)
     return (
-        <div>
+        <div className="auth-container">
             {error && <p>{error}</p>}   
             {loading ? <h2>Loading...</h2> :  <div>{user ? <Dashboard /> : <Auth />}</div>}
         </div>
