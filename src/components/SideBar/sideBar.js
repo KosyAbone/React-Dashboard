aa398ce1be4da0e090b91f2e9ddd4be7af495e01
Kosiso Abone <abonekosiso@gmail.com>
2023-10-25 10:03:49 -0400

    made sidenav links active when each side-nav page is rendered

--- src/components/SideBar/sideBar.js
+++ src/components/SideBar/sideBar.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sideBar.css';
 import { Link } from 'react-router-dom';
 import { useUserContext } from '../../context/userContext'; 
 
 const Sidebar = () => {
   const { logoutUser } = useUserContext();
+
+  const [activeItem, setActiveItem] = useState(null);
+
+  const handleNavItemClick = (route) => {
+    setActiveItem(route);
+  };
+  
   return (
     <div className="sidebar">
-        <Link to="/login/profile" className="sidebar-item">Profile</Link>
-        <Link to="/login/map" className="sidebar-item" >Map</Link>
-        <Link to="/login/news-feed" className="sidebar-item">News Feed</Link>
-        <Link to="/login/weather" className="sidebar-item">Weather</Link>
-        <Link to="/login/tools" className="sidebar-item">Tools</Link>
+        <Link 
+          to="/login/profile" 
+          className={`sidenav-item ${activeItem === '/login/profile' ? 'active' : ''}`}
+          onClick={() => handleNavItemClick('/login/profile')}
+        >
+          Profile
+        </Link>
+
+        <Link 
+          to="/login/map" 
+          className={`sidenav-item ${activeItem === '/login/map' ? 'active' : ''}`}
+          onClick={() => handleNavItemClick('/login/map')}
+        >
+          Map
+        </Link>
+
+        <Link 
+          to="/login/news-feed"  
+          className={`sidenav-item ${activeItem === '/login/news-feed' ? 'active' : ''}`}
+          onClick={() => handleNavItemClick('/login/news-feed')}
+        >
+          News Feed
+        </Link>
+
+        <Link 
+          to="/login/weather" 
+          className={`sidenav-item ${activeItem === '/login/weather' ? 'active' : ''}`}
+          onClick={() => handleNavItemClick('/login/weather')}
+        >
+          Weather
+        </Link>
+
+        <Link 
+          to="/login/tools" 
+          className={`sidenav-item ${activeItem === '/login/tools' ? 'active' : ''}`}
+          onClick={() => handleNavItemClick('/login/tools')}
+        >
+          Tools
+        </Link>
+
         <button className='logoutBtn' onClick={() => logoutUser()}>Logout</button>
     </div>
   );
